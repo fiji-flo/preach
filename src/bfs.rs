abd9e35edaede9c31d1872819cef77cd0eb689ba
Florian Merz <flomerz@gmail.com>
2016-04-28 23:46:49 +0200

    fix bfs (kinda)

--- src/bfs.rs
+++ src/bfs.rs
@@ -1,14 +1,27 @@
 use std::collections::VecDeque;
-pub fn bfs<F, GT: ::graph::GraphTraversal>(g: &GT, s: usize, mapped_function: &F ,marker: &mut ::mark::Marker) where F: Fn(usize) {
+pub fn bfs<F, GT: ::graph::GraphTraversal>(g: &GT, s: usize, mapped_function: &mut Box<F> ,marker: &mut ::mark::Marker) where F: FnMut(usize) {
     let mut q: VecDeque<usize> = VecDeque::new();
     q.push_back(s);
     marker.mark(s);
     loop {
         if let Some(u) = q.pop_front() {
             mapped_function(u);
-            q.extend(g.fwd_edges(u).iter().filter(|&x| marker.is_marked(*x)));
+            q.extend(g.fwd_edges(u).iter().filter_map(|&x| {
+                if !marker.is_marked(x) {
+                    marker.mark(x);
+                    Some(x)
+                } else {
+                    None
+                }
+            }));
         } else {
             break;
         }
+    }
+}
+
+pub fn full_bfs<F, GT: ::graph::GraphTraversal>(g: &GT, mapped_function: &mut Box<F>, marker: &mut ::mark::Marker) where F: FnMut(usize) {
+    for v in g.sources() {
+        bfs(g, v, mapped_function, marker);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ fn main() {
         let g = graphreader::new_from_file(&(arg.unwrap())).unwrap();
         let mut marker = mark::TMarker::new(g.len());
         //dfs::dfs(&g, 0, &(Box::new(|x: usize| print!("{} ", x))), &mut marker);
+        println!("doint a full dfs:");
         dfs::full_dfs(&g, &mut (Box::new(|x: usize| print!("{} ->", x))), &mut marker);
-        //bfs::bfs(&g, 0, &(|x: usize| print!("{} ", x)), &mut marker);
+        marker.reset();
+        println!("doint a full bfs:");
+        bfs::full_bfs(&g, &mut (Box::new(|x: usize| print!("{} ->", x))), &mut marker);
         println!("done!");
     }
 }
